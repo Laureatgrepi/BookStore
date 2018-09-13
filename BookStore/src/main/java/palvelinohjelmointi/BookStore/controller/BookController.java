f70c871db735723355d650a837434aadf3d39d4d
Laureat Grepi <laureatgrepi@hotmail.com>
2018-09-13 11:21:59 +0300

    ei mitää uutta

--- BookStore/src/main/java/palvelinohjelmointi/BookStore/controller/BookController.java
+++ BookStore/src/main/java/palvelinohjelmointi/BookStore/controller/BookController.java
@@ -2,6 +2,7 @@ package palvelinohjelmointi.BookStore.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
