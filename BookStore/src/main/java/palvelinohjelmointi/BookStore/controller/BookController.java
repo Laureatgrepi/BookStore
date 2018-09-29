beb8bc1936e219b57ad3f37a1e3a7cfdea64ec08
Laureat Grepi <laureatgrepi@hotmail.com>
2018-09-29 14:30:21 +0300

    pari rest-metodia

--- BookStore/src/main/java/palvelinohjelmointi/BookStore/controller/BookController.java
+++ BookStore/src/main/java/palvelinohjelmointi/BookStore/controller/BookController.java
@@ -1,6 +1,7 @@
 package palvelinohjelmointi.BookStore.controller;
 
-
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,60 +9,67 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import palvelinohjelmointi.BookStore.domain.Book;
 import palvelinohjelmointi.BookStore.domain.BookRepository;
 import palvelinohjelmointi.BookStore.domain.CategoryRepository;
 
-
 @Controller
 public class BookController {
-	
+
 	@Autowired
 	private BookRepository bookRepository;
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
 
-
-@GetMapping("/index")
+	@GetMapping("/index")
 	public String bookStore() {
-	return "indexPage";
-}
+		return "indexPage";
+	}
 
-@GetMapping("/booklist")
+	@GetMapping("/booklist")
 	public String bookList(Model model) {
-		model.addAttribute("booklist", bookRepository.findAll() );
-	return "booklist";
-}
-@GetMapping("/delete/{id}")
+		model.addAttribute("booklist", bookRepository.findAll());
+		return "booklist";
+	}
+
+	@GetMapping("/delete/{id}")
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		bookRepository.deleteById(bookId);
 		return "redirect:../booklist";
-		
-}
-@GetMapping("/edit/{id}")
-public String editBook(@PathVariable("id") Long bookId, Model model){
-	model.addAttribute("book", bookRepository.findById(bookId));
-	model.addAttribute("categories", categoryRepository.findAll());
 
-	return "editBook";
-	
+	}
 
-}
-	
+	@GetMapping("/edit/{id}")
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", bookRepository.findById(bookId));
+		model.addAttribute("categories", categoryRepository.findAll());
 
-@GetMapping("/create")
+		return "editBook";
+
+	}
+
+	@GetMapping("/create")
 	public String createBook(Model model) {
-		model.addAttribute("book",new Book());
+		model.addAttribute("book", new Book());
 		model.addAttribute("categories", categoryRepository.findAll());
 		return "createBook";
-}
+	}
 
-@PostMapping("/save")
-public String saveBook(Book book) {
+	@PostMapping("/save")
+	public String saveBook(Book book) {
 		bookRepository.save(book);
 		return "redirect:booklist";
-}
+	}
+	@GetMapping("/books")
+	public @ResponseBody List<Book> bookListRest(){
+		return (List<Book>) bookRepository.findAll();
+	}
+	@GetMapping("/book/{id}")
+	public @ResponseBody Optional<Book> findBookById(@PathVariable("id") Long bookId){
+		return bookRepository.findById(bookId);
+		
+	}
 }
