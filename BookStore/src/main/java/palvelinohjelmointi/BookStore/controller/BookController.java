1365053ef809bc1c9c9db0837dcfd37a51fd94e9
Laureat Grepi <laureatgrepi@hotmail.com>
2018-10-03 22:36:09 +0300

    kirjan lisääminen restin avulla

--- BookStore/src/main/java/palvelinohjelmointi/BookStore/controller/BookController.java
+++ BookStore/src/main/java/palvelinohjelmointi/BookStore/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -63,7 +64,7 @@ public class BookController {
 		bookRepository.save(book);
 		return "redirect:booklist";
 	}
-	@GetMapping("/books")
+	@GetMapping ( "/books")
 	public @ResponseBody List<Book> bookListRest(){
 		return (List<Book>) bookRepository.findAll();
 	}
