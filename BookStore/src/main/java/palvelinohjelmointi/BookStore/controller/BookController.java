6f864898cbf0a3ec4b9813183965a09bda5f82a2
Laureat Grepi <laureatgrepi@hotmail.com>
2018-10-06 13:29:50 +0300

    config-luokka ja pari käyttäjää

--- BookStore/src/main/java/palvelinohjelmointi/BookStore/controller/BookController.java
+++ BookStore/src/main/java/palvelinohjelmointi/BookStore/controller/BookController.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import palvelinohjelmointi.BookStore.domain.Book;
@@ -24,6 +24,11 @@ public class BookController {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	   @RequestMapping(value="/login")
+	    public String login() {	
+	        return "login";
+	    }	
 
 	@GetMapping("/index")
 	public String bookStore() {
