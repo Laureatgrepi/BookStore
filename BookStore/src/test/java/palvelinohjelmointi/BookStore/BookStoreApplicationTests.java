e62d7549136ada4627f733efb80399e4dbfe4f6a
Laureat Grepi <laureatgrepi@hotmail.com>
2018-10-29 08:41:50 +0200

    eka testi

--- BookStore/src/test/java/palvelinohjelmointi/BookStore/BookStoreApplicationTests.java
+++ BookStore/src/test/java/palvelinohjelmointi/BookStore/BookStoreApplicationTests.java
@@ -1,16 +1,25 @@
 package palvelinohjelmointi.BookStore;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import palvelinohjelmointi.BookStore.controller.BookController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookStoreApplicationTests {
 
+	@Autowired
+	private BookController bookController;
+	
 	@Test
 	public void contextLoads() {
+		assertThat(bookController).isNotNull();	
 	}
 
 }
